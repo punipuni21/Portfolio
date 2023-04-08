8a3e90c29e3e5ff5d4170c2fa8711eb0c35d8336
Ryotaro Watanabe <46588556+punipuni21@users.noreply.github.com>
2023-04-08 23:56:57 +0900

    Revert "デザインを修正 (#30)"
    This reverts commit b71784ef766f4f02f4fa32878b8f9a89346ed494.

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -5,7 +5,6 @@ import Footer from "../src/components/Footer/Footer";
 import ProjectCard from "../src/components/ProjectCard/ProjectCard";
 import SkillCard from "../src/components/SkillCard/SkillCard";
 import SocialMediaIcon from "../src/components/SocialMediaIcon/SocialMediaIcon";
-import styles from "./TopPage.module.css";
 
 const IndexPage: React.FC = () => {
   // この配列にプロジェクト情報を入れていきます。
@@ -54,13 +53,13 @@ const IndexPage: React.FC = () => {
   ];
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>ポートフォリオ</title>
         <meta name="description" content="ポートフォリオサイトです。" />
       </Head>
       <Header />
-      <main className={styles.content}>
+      <main>
         <section>
           <h1>自己紹介</h1>
           <p>
@@ -68,8 +67,8 @@ const IndexPage: React.FC = () => {
           </p>
         </section>
         <section>
-          <h2 className={styles.h2}>プロジェクト一覧</h2>
-          <div className={styles.projects}>
+          <h2>プロジェクト一覧</h2>
+          <div className="projectContainer">
             {projects.map((project) => (
               <ProjectCard key={project.id} project={project} />
             ))}
@@ -77,7 +76,7 @@ const IndexPage: React.FC = () => {
         </section>
         <section>
           <h2>スキル一覧</h2>
-          <div className={styles.skills}>
+          <div className="skillsContainer">
             {skills.map((skill) => (
               <SkillCard key={skill.name} skill={skill} />
             ))}
@@ -96,7 +95,7 @@ const IndexPage: React.FC = () => {
         </section>
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
